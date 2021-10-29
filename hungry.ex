85986b14a20c03dd54a2d0b3a7f791ab832a693d
Saad ur Rehman <SaadShanghai244@gmail.com>
2021-10-29 15:18:24 +0500

    added if -else condition

--- hungry.ex
+++ hungry.ex
@@ -1,1 +1,10 @@
-IO.inspect("Hello World")
+defmodule M do
+  def change(num) do
+   if num == 5 do
+    IO.inspect("its five")
+    else
+    IO.inspect("its other than 5")
+    end
+  end
+end
+M.change(10)
